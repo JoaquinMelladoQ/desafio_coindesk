5c4cc3cb1c174bd45b749db17728c7a766046984
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-08 10:10:25 -0300

    changing values and push them into an empty array

--- coindesk.rb
+++ coindesk.rb
@@ -17,6 +17,16 @@ data = JSON.parse(data_api)
 
 # puts data.class
 
-data_hash = data["bpi"]["2020-08-14"]
+data_hash = data["bpi"]
 
-puts data_hash
+# puts data_hash
+
+filtered_data = []
+data_hash.each do | key, value |
+    if value > 5000
+        filtered_data.push( value )
+    end
+
+end
+
+print filtered_data
